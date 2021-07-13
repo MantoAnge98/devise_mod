40c90c88dbd7734741e9abce13cf2cad757111f6
AngeManto <koutikaangemarie@gmail.com>
2021-07-13 13:01:28 +0000

    create application mailer

--- app/mailers/meetings_mailer.rb
+++ app/mailers/meetings_mailer.rb
@@ -1,2 +1,11 @@
 class MeetingsMailer < ApplicationMailer
+  
+  def contact_mail(meeting)
+    @meeting      = params[:id]
+    @name         = @meeting.name
+    @description  = @meeting.description
+    @date         = @meeting.date
+    mail(to: "koutikaangemarie@gmail.com", subject: 'Meeting Create !')
+  end
+
 end
